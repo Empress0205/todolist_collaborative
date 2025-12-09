a4441d724ce54f7ab00bf34a1297c8d5d4b4f977
Edmund Rwegasira <edmundrwegasira@outlook.com>
2025-12-09 16:45:33 +0300

    Added delete & update functionality

--- BACKEND/To_do_list/views.py
+++ BACKEND/To_do_list/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
+@api_view(['DELETE'])
+def delete_task(request,id):
+    task = get_object_or_404(Task,id=id)
+    task.delete()
+    return Response(
+            {"message":"Task deleted successfully"},
+             status=status.HTTP_204_NO_CONTENT   
+    )            
+
+@api_view(['PUT'])
+def update_task(request,id):
+    task = get_object_or_404(Task,id=id)
+    serializer = TaskSerializer(task,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
