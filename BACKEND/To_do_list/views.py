25df1d21b3b0bf1baf6165dbb8cbbc1184253218
Habrovan004 <habrovan14@gmail.com>
2025-12-09 19:23:01 +0300

    Add task create API endpoints

--- BACKEND/To_do_list/views.py
+++ BACKEND/To_do_list/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_task(request):
+
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
